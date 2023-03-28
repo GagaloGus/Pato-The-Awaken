11ce8025fda755bee580a938ef5bd221ed3a1083
GagaloGus <gabriel.mendez.sas@gmail.com>
2023-03-28 20:21:41 +0200

    mesa randomizado arreglao y aparcao

--- Assets/Scripts/Desk_Random.cs
+++ Assets/Scripts/Desk_Random.cs
@@ -5,19 +5,28 @@ using UnityEngine;
 public class Desk_Random : MonoBehaviour
 {
     public GameObject[] allDesks, shuffledDesks;
+    public GameObject mesaInicio, mesaFinal;
     public int amountDesksGenerated;
     // Start is called before the first frame update
     void Start()
     {
         //hace que el tamaño del array de Shuffle se cambie al que queremos automaticamente
-        GameObject[] resizeArray = new GameObject[amountDesksGenerated];
+        GameObject[] resizeArray = new GameObject[amountDesksGenerated + 2];
         shuffledDesks.CopyTo(resizeArray, 0); shuffledDesks = resizeArray;
 
+        shuffledDesks[0] = mesaInicio; shuffledDesks[shuffledDesks.Length - 1] = mesaFinal;
+
+        int rnd_aux = 0;
         //randomiza las mesas generadas
-        for(int i = 0; i < amountDesksGenerated; i++)
+        for(int i = 1; i < amountDesksGenerated + 1; i++)
         {
-            int rnd = Random.Range(0, allDesks.Length);
-            shuffledDesks[i] = allDesks[rnd];
+                int rnd = rnd_aux;
+                do
+                {
+                    rnd = Random.Range(0, allDesks.Length);
+                } while (rnd == rnd_aux);
+                rnd_aux = rnd;
+                shuffledDesks[i] = allDesks[rnd];
         }
 
         //instancia las mesas generadas para que esten en fila
